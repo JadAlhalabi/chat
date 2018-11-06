d2f068ab32d0212b325d8434069d0320284c77c2
Jad Alward Alhalabi <0007jaal@res.ludvika.intra>
2018-11-06 15:51:00 +0100

    lektion tisdag

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,22 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
+// TODO: Läsa in input till JS
+// TODO: Konvertera till JSON
+// TODO: Skicka till server
+// TODO: Hämta meddelanden från servern
+// TODO: Konvertera JSON till JS
+// TODO: Skriva ut meddelanden
+// TODO: Kunna välja ett användernamn
+// TODO: Chatt user interface (html, css)
+// TODO: Skicka meddelander med enter
+// TODO: Läsa in chatt historik
+// TODO: Användare ska ha olika färger
+// TODO: Man ska kunna se vem som skicka ett meddelande
+// TODO: Kunna se vilken tid ett meddelande skickades
+// TODO: Enklare dokumentation
+const button = document.getElementById();
+button.addEventListener('click', event => {
+  const textinput = document.getElementById('inputtext');
+  console.log(textinput.value);
+});
