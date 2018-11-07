45128312e496f17833e2d1d510896b3150274b0c
Jad Alward Alhalabi <0007jaal@res.ludvika.intra>
2018-11-07 11:43:01 +0100

    onsdag lektion

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,6 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
-// TODO: Läsa in input till JS
-// TODO: Konvertera till JSON
-// TODO: Skicka till server
-// TODO: Hämta meddelanden från servern
-// TODO: Konvertera JSON till JS
 // TODO: Skriva ut meddelanden
 // TODO: Kunna välja ett användernamn
 // TODO: Chatt user interface (html, css)
@@ -15,8 +10,28 @@ import style from '../css/style.css';
 // TODO: Man ska kunna se vem som skicka ett meddelande
 // TODO: Kunna se vilken tid ett meddelande skickades
 // TODO: Enklare dokumentation
-const button = document.getElementById();
-button.addEventListener('click', event => {
-  const textinput = document.getElementById('inputtext');
-  console.log(textinput.value);
+import password from './config';
+
+const connection = new WebSocket('ws://104.248.143.87:1337');
+connection.onmessage = message => {
+  const obj = JSON.parse(message.data);
+  console.log(obj.data.text);
+  const temp = document.getElementById('temp');
+  const clone = document.importNode(temp.content.firstElementChild, true);
+  clone.textContent = obj.data.text;
+  const paste = document.getElementById('paste');
+  paste.appendChild(clone);
+};
+
+const btnEl = document.getElementById('btn');
+btnEl.addEventListener('click', event => {
+  event.preventDefault();
+  const textinput = document.getElementById('msg');
+  const obj = {
+    type: 'Message',
+    data: textinput.value,
+    key: password
+  };
+  const JsonObj = JSON.stringify(obj);
+  connection.send(JsonObj);
 });
