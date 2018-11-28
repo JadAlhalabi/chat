5a4fc2e9379b7175e1cca65b9af4f3854776734a
Jad Alward Alhalabi <0007jaal@res.ludvika.intra>
2018-11-28 11:38:41 +0100

    28/11

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,43 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
+
 // TODO: Chatt user interface (html, css)
-// TODO: Skicka meddelander med enter
-// TODO: Läsa in chatt historik
 // TODO: Användare ska ha olika färger
 // TODO: Man ska kunna se vem som skicka ett meddelande
 // TODO: Kunna se vilken tid ett meddelande skickades
 // TODO: Enklare dokumentation
 import password from './config';
 
+function printmsg(printer, author, time) {
+  const temp = document.getElementById('temp');
+  const clone = document.importNode(temp.content.firstElementChild, true);
+  clone.textContent = ` ${new Date(time)} ${author} ${printer}`;
+  const paste = document.getElementById('paste');
+  paste.appendChild(clone);
+}
+
 const connection = new WebSocket('ws://104.248.143.87:1337');
 connection.onmessage = message => {
   const obj = JSON.parse(message.data);
   console.log(obj.data.text);
-  console.log(obj.type);
+  console.log(obj);
+  console.log(obj.data.time);
 
   if (obj.type === 'color') {
     const msg = document.getElementById('msg');
     msg.setAttribute('placeholder', 'Input message');
   }
 
-  const temp = document.getElementById('temp');
-  const clone = document.importNode(temp.content.firstElementChild, true);
-  clone.textContent = obj.data.text;
-  const paste = document.getElementById('paste');
-  paste.appendChild(clone);
+  if (obj.type === 'history') {
+    console.log(obj.data);
+    const history = obj.data;
+    history.forEach(element => {
+      console.log(element.text);
+      printmsg(element.text, element.author, element.time);
+    });
+  }
+  printmsg(obj.data.text, obj.data.author, obj.data.time);
 };
 
 const btnEl = document.getElementById('btn');
